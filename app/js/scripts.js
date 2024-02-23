95d0cbe16de5866e8e1bfdc0c0a3a8f4ea092494
AlexGil812 <alexgil3399@gmail.com>
2024-02-22 19:45:04 -0500

    initial commit

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,41 +1,60 @@
+// declare variables
 var tabs = document.querySelectorAll("nav a");
 var contentPara = document.querySelector(".content");
 
-function makeActive(event) {
-  if (!event.target.matches("nav a")) return;
+// add the class active to one tab
+function makeActive(currentHash) {
   makeInactive();
-  event.target.classList.add("active");
+  var tabToActivate = document.querySelector(`a[href="#${currentHash}"]`);
+  tabToActivate.classList.add("active");
 }
 
+// remove the class active from all tabs
 function makeInactive() {
-    for (let i = 0; i < tabs.length; i++) {
+  for (let i = 0; i < tabs.length; i++) {
     tabs[i].classList.remove("active");
   }
 }
 
+// runs on page load and whenever the hash changes
+//function setContentAccordingToHash() {
+  var currentHash = window.location.hash.substring(1);
+  contentPara.innerHTML = data[currentHash];
+  makeActive(currentHash);
+//}
 
+
+
+// runs on page load and whenever the hash changes
 function setContentAccordingToHash() {
   const currentHash = window.location.hash.substring(1);
-  contentPara.innerHTML = data[currentHash];
+  for (var i = 0; i < data.length; i++) {
+    if (data[i].section === currentHash) {
+
+     
+
+
+
+
+      contentPara.innerHTML = `<h2>${data[i].section}</h2> <p>${data[i].story}</p>`;
+      makeActive(currentHash);
+    }
+  }
 }
 
 
+
+
+// only runs once on page load
 function initializePage() {
-    // set a default if there is no hash
-    if (!window.location.hash) {
-      window.location.hash = "cuisines";
-      document.querySelector('nav a').classList.add("active");
-    } else {
-      // if there is a hash set the active tab according to the hash
-      document
-        .querySelector('[href="' + window.location.hash + '"] ')
-        .classList.add("active");
-    }
-    setContentAccordingToHash();
+  if (!window.location.hash) {
+    window.location.hash = "cuisines";
+    document.querySelector('[href="#cuisines"]').classList.add("active");
   }
+  setContentAccordingToHash();
+}
 
-document.addEventListener("click", makeActive);
-window.addEventListener("hashchange", setContentAccordingToHash); 
+window.addEventListener("hashchange", setContentAccordingToHash);
 
-initializePage(); 
-
+// initializePage()
+document.addEventListener("DOMContentLoaded", initializePage);
